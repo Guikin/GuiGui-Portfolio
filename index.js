2b4b57febd2f6f96649139eb080e1c57b4daba74
GuiGui <jeanguillaumethomas3@gmail.com>
2022-02-16 18:40:42 -0800

    Javascript functionality

---
+++ index.js
@@ -0,0 +1,32 @@
+const faders = document.querySelectorAll(".faders")
+
+const Options= {
+    threshold:.5,
+}
+
+const sectionTwoObserver = new IntersectionObserver(function(entries,sectionTwoObserver){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add("appear")
+            sectionTwoObserver.unobserve(entry.target);
+        }
+    })
+},Options);
+
+faders.forEach(fader=>{
+    sectionTwoObserver.observe(fader);
+});
+
+
+window.addEventListener("scroll",bgChange)
+function bgChange(){
+    if(window.scrollY>0){
+    document.querySelector("nav").classList.add("bg-dark")
+    document.querySelector("nav").classList.remove("background-col")}
+    else{
+        document.querySelector("nav").classList.remove("bg-dark")
+    document.querySelector("nav").classList.add("background-col")
+    }
+}
